976c3e361131a65171cab3e44d50f670f17c3f64
nerdycat99 <nevillewood@nevilles-imac.home>
2018-03-01 13:01:27 +0000

    basic working app - no rules for moves and notes

--- src/MoveRules.js
+++ src/MoveRules.js
@@ -5,5 +5,38 @@ class MoveRules extends Component {
 
 export const pieceValid = (current)=>{
     alert(current.row);
+
+    // get piece and direct
 }
+/*
+All
+check it's your go done in method that calls this
+check don't take your own piece
+check no pieces between current and target
+determine if diagonal move
+determine if horizontal move
+determine if vertical move
+
+
+how to take another piece (capture)
+does it make check
+does it make stalemate
+does it make checkmate
+
+
+
+Pawn 
+move 2 on first move only
+only move 1 and forward horizontal
+pawn promotion
+Take if 1 space diagonal
+
+
+
+
+
+*/
+
+
+
 
